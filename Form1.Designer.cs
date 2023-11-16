a899ae6833424179b453b172e3e508bf53e5291c
Berke <116177776+berkeyucetas@users.noreply.github.com>
2023-11-16 19:53:46 +0300

    Plaka Numarasina Gore Sehirin Adi Bir Gunde Bir Proje  44

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Plaka_Numarasına_Göre_Şehirin_Adı
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textPlaka = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textSehir = new System.Windows.Forms.TextBox();
+            this.buttonGetir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Plaka:";
+            // 
+            // textPlaka
+            // 
+            this.textPlaka.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textPlaka.Location = new System.Drawing.Point(91, 26);
+            this.textPlaka.Name = "textPlaka";
+            this.textPlaka.Size = new System.Drawing.Size(134, 30);
+            this.textPlaka.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Şehir Adı:";
+            // 
+            // textSehir
+            // 
+            this.textSehir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textSehir.Location = new System.Drawing.Point(126, 169);
+            this.textSehir.Name = "textSehir";
+            this.textSehir.Size = new System.Drawing.Size(201, 30);
+            this.textSehir.TabIndex = 3;
+            // 
+            // buttonGetir
+            // 
+            this.buttonGetir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonGetir.Location = new System.Drawing.Point(17, 89);
+            this.buttonGetir.Name = "buttonGetir";
+            this.buttonGetir.Size = new System.Drawing.Size(264, 41);
+            this.buttonGetir.TabIndex = 4;
+            this.buttonGetir.Text = "Şehir Getir";
+            this.buttonGetir.UseVisualStyleBackColor = true;
+            this.buttonGetir.Click += new System.EventHandler(this.buttonGetir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 231);
+            this.Controls.Add(this.buttonGetir);
+            this.Controls.Add(this.textSehir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textPlaka);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Şehir";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textPlaka;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textSehir;
+        private System.Windows.Forms.Button buttonGetir;
+    }
+}
+
